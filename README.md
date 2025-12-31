42d6825ed8751b36edab674e78c3c2d468b0d6d1
Golu kumar <golu6205623560@gmail.com>
2025-12-31 09:06:04 +0530

    Initial commit: Keeper App using React

---
+++ README.md
@@ -0,0 +1,18 @@
+# Keeper App (React)
+
+A simple Google Keep–style notes app built using React.
+
+## Tech Stack
+- React
+- JavaScript
+- Vite
+
+## How to Run Locally
+```bash
+npm install
+npm run dev
+
+## MSDIAN
+
+> "TILL THE FULL STOP DOESN'T COME, SENTENCE IS NOT COMPLETE."
+
---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Header.jsx";
+import Note from "./Note.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
